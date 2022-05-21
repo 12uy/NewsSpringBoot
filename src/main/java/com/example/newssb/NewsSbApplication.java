9ac025dd2bb33654d65f5590350d7745c7fe9e2f
RUY <Dovanduy.learn@gmail.com>
2022-05-21 07:54:25 +0700

    first commit

---
+++ src/main/java/com/example/newssb/NewsSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.newssb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newssb/NewsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newssb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
